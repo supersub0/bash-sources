3c17ac57e0766ab0faa135694684823caa2156db
Robert Gross <robert.gross@tradebyte.com>
2021-10-01 11:02:00 +0200

    adjust prompt

--- prompt.bash
+++ prompt.bash
@@ -1,22 +1,19 @@
 # prompt
 if [ -n "$force_color_prompt" ]; then
     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	# We have color support; assume it's compliant with Ecma-48
-	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-	# a case would tend to support setf rather than setaf.)
-	color_prompt=yes
+        color_prompt=yes
     else
-	color_prompt=
+	      color_prompt=
     fi
 fi
 
 parse_git_branch() {
     BRANCHES=$(git branch -qv 2> /dev/null)
-    
+
     if [[ $BRANCHES == *"* (HEAD"* ]]; then
-        echo "$BRANCHES" | sed -e '/^[^*]/d' -e 's/\* \([^\s]*\) \([^\s]*\) .*/(\2)/'
+        echo "$BRANCHES" | sed -e '/^[^*]/d' -e 's/\* [^\)]*) \([^ ]*\) .*/\1/'
     else
-        git branch -q 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+        echo "$BRANCHES" | sed -e '/^[^*]/d' -e 's/\* \([^ ]*\) .*/\1/'
     fi
 }
 
@@ -25,7 +22,7 @@ parse_short_path () {
 }
 
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}$(now)|\[\033[01;32m\]\u@\h\[\033[00m\]|\[\033[01;34m\]$(parse_short_path \w \W)\[\033[00m\]|\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\n\$ '
+    PS1='${debian_chroot:+($debian_chroot)}$(now)|\[\033[01;32m\]\u@\h\[\033[00m\]|\[\033[01;34m\]$(parse_short_path \w \W)\[\033[00m\]|\[\033[01;31m\]$(parse_git_branch)\n\[\033[01;36m\]\$\[\033[00m\] '
 else
     PS1='${debian_chroot:+($debian_chroot)}$(now)|\u@\h|$(parse_short_path \w \W)|$(parse_git_branch)\n\$ '
 fi
